b62f9708573416a28b9046f5c5c8e0069d05b74b
DemonicRous <114003225+DemonicRous@users.noreply.github.com>
2023-06-27 19:35:43 +0500

    Updated

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,27 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
-        activeMode: "Обычный"
+        activeMode: "Обычный",
+        themes: ["light","dark","cupcake","bumblebee","emerald","corporate","synthwave","retro","cyberpunk","valentine","halloween","garden","forest","aqua","lofi","pastel","fantasy","wireframe","black","luxury","dracula","cmyk","autumn","business","acid","lemonade","night","coffee","winter",],
+        activeTheme: "dark",
       }
     },
     methods: {
-     
+     setTheme: function(theme) {
+      document.querySelector('html').dataset.theme = theme;
+     }
     },
     mounted() {
-     
+
+      function activateTheme(theme) {
+        document.querySelector('html').dataset.theme = theme;
+      }
+
+      if (localStorage.activeTheme) {
+        activateTheme(localStorage.getItem('activeTheme'));
+      } else {
+        localStorage.setItem('activeTheme', this.activeTheme);
+        activateTheme(localStorage.getItem('activeTheme'));
+      }
     }
   }).mount('#app')
