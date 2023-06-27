75fa8c40ac6e822b3aaa6eed4369923b3367a243
DemonicRous <114003225+DemonicRous@users.noreply.github.com>
2023-06-27 18:54:33 +0500

    Created

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+  createApp({
+    data() {
+      return {
+        activeMode: "Обычный"
+      }
+    },
+    methods: {
+     
+    },
+    mounted() {
+     
+    }
+  }).mount('#app')
