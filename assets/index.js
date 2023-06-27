122f4b5fdf01c92c3cb7c92efa41ad4a0be13bed
DemonicRous <114003225+DemonicRous@users.noreply.github.com>
2023-06-27 23:24:39 +0500

    Updated

--- assets/index.js
+++ assets/index.js
@@ -7,6 +7,7 @@ const { createApp } = Vue;
         themes: ["light","dark","cupcake","bumblebee","emerald","corporate","synthwave","retro","cyberpunk","valentine","halloween","garden","forest","aqua","lofi","pastel","fantasy","wireframe","black","luxury","dracula","cmyk","autumn","business","acid","lemonade","night","coffee","winter",],
         activeTheme: "dark",
         modeTheme: localStorage.getItem('activeTheme'),
+        modeMemory: false,
       }
     },
     methods: {
@@ -14,6 +15,105 @@ const { createApp } = Vue;
       document.querySelector('html').dataset.theme = theme;
       localStorage.setItem('activeTheme', theme);
       this.modeTheme = localStorage.getItem('activeTheme')
+     },
+     simpleСheckInput: function() {
+      let input = document.getElementById('inputCalc');
+      input.value.replace('= ', '');
+      input.value = input.value.replace(/[^0-9\.\,\-+/*()%√]/g, '');
+     },
+     simpleCalcClearSymbol: function() {
+      let input = document.getElementById('inputCalc');
+      input.value.replace('= ', '');
+      input.value = input.value.replace(/[^0-9\.\,\-+/*()%√]/g, '');
+     },
+     simpleKeyboard: function(key) {
+      let input = document.getElementById('inputCalc');
+      let inputAfter = document.getElementById('inputAfter');
+      switch (key) {
+        case 1:
+          input.value += key;
+          break;
+        case 2:
+          input.value += key;
+          break;
+        case 3:
+          input.value += key;
+          break;
+        case 4:
+          input.value += key;
+          break;
+        case 5:
+          input.value += key;
+          break;
+        case 6:
+          input.value += key;
+          break;
+        case 7:
+          input.value += key;
+          break;
+        case 8:
+          input.value += key;
+          break;
+        case 9:
+          input.value += key;
+          break;
+        case 0:
+          input.value += key;
+          break;
+        case '+':
+          if (!isNaN(input.value.slice(-1)) || (input.value == "")) {
+            input.value += key;
+          }
+          break;
+        case '-':
+          if (!isNaN(input.value.slice(-1)) || (input.value == "")) {
+            input.value += key;
+          }
+          break;
+        case '*':
+          if (!isNaN(input.value.slice(-1)) || (input.value == "")) {
+            input.value += key;
+          }
+          break;
+        case '/':
+          if (!isNaN(input.value.slice(-1)) || (input.value == "")) {
+            input.value += key;
+          }
+          break;
+        case ',':
+          if (!isNaN(input.value.slice(-1)) && !(input.value == "") && !(input.value.length == 0) && !(input.value == null)) {
+            input.value += '.';
+          }
+          break;
+        case '=':
+
+          //function isInteger(x) { return typeof x === "number" && isFinite(x) && Math.floor(x) === x; }
+          function isFloat(x) { return !!(x % 1); }
+
+          if (!isNaN(input.value.slice(-1)) || input.value.slice(-1) === '.' || input.value == "") {
+            
+            if (!(eval(input.value) === undefined)) {
+              if (isFloat(eval(input.value))) {
+                inputAfter.value = input.value;
+                input.value = '= ' + Math.round(eval(input.value) * 100) / 100;
+              } else {
+                inputAfter.value = input.value;
+                input.value = '= ' + eval(input.value);
+              }
+              
+            } else {
+              inputAfter.value = 0;
+              input.value = '= ' + 0;
+            }
+          
+          } else {
+            console.log('Error')
+          }
+          
+          break;
+        default:
+          alert( "Нет таких значений" );
+      }
      }
     },
     mounted() {
@@ -28,5 +128,8 @@ const { createApp } = Vue;
         localStorage.setItem('activeTheme', this.activeTheme);
         activateTheme(localStorage.getItem('activeTheme'));
       }
+
+      
+
     }
   }).mount('#app')
